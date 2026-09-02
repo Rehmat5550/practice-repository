aeed28708744ab5e1299b416e6e358937d480622
Ali Muhammad <alimuhammad5532@gmail.com>
2026-09-02 11:16:18 +0500

    add one html file and do some changes in python file

--- python-practice.py
+++ python-practice.py
@@ -3,3 +3,7 @@ class Calculator:
         return a + b
     def division(self, a : int, b: int ) -> float:
         return a / b
+
+calc = Calculator()
+print(calc.add(4, 45))
+print(calc.division(4, 23))
